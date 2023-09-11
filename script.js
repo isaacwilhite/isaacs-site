ea6d162e3afa628c4aad028861ff94b7d6fcb769
Isaac Wilhite <isaacwilhite987@gmail.com>
2023-09-10 20:38:27 -0600

    updating

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "The things that drive, motivate, and bring joy to my life!";
 document.querySelector("body").appendChild(h2);
